52415920bdda3c88677dcbea93edb6c1576df5c3
irfan irawan <irfan.irawan@tokodistributor.com>
2019-10-03 22:51:48 +0700

    init

--- explore_layout/lib/main.dart
+++ explore_layout/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Building layouts'),
         ),
-        body: myLayoutWidgetCombineWithPadding(),
+        body: myLayoutWidgetContainer(),
       ),
     );
   }
@@ -43,6 +43,51 @@ class MyApp extends StatelessWidget {
         style: TextStyle(
           fontSize: 24.0,
           color: Colors.lime
+        ),
+      ),
+    );
+  }
+
+  // center layout widget
+  Widget myLayoutWidgetCenter() {
+    return Center(
+      child: Text('Irfan sedang ada ditengah layar', 
+      style: TextStyle(
+          fontSize: 24.0
+        )
+      )
+    );
+  }
+
+  // top center layout widget using Alignment widget
+  Widget myLayoutWidgetTopCenter() {
+    return Align(
+      // using value -1, 0, 1 for widget position
+      // alignment: Alignment(0, -1),
+      // or using another trick
+      alignment: Alignment.topCenter,
+      child: Text('Irfan top center',
+      style: TextStyle(
+          fontSize: 24.0
+        )
+      )
+    );
+  }
+
+  // container widget using BoxDecoration widget for custom style
+  Widget myLayoutWidgetContainer() {
+    return Container(
+      margin: EdgeInsets.all(30.0),
+      padding: EdgeInsets.all(10.0),
+      alignment: Alignment.topCenter,
+      width: 200,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.green,
+        border: Border.all()
+      ),
+        child: Text('Hello Container widget', style: TextStyle(
+          fontSize: 24.0
         ),
       ),
     );
